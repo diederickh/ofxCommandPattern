267461c23ae46a4e19305353cf85411f75a12457
roxlu <diederick@apollomedia.nl>
2010-09-20 17:17:18 +0200

    Porting to windows

--- src/ofxCommandProcessor.h
+++ src/ofxCommandProcessor.h
@@ -1,14 +1,10 @@
 #ifndef OFXCOMMANDPROCESSOR
 #define OFXCOMMANDPROCESSOR
 
-//#include "ofxThread.h"
 #include <deque>
 #include <iostream>
-#undef check
-#include <boost/thread.hpp>
 
 class ofxCommand;
-//class ofxCommandProcessor : public ofxThread {
 class ofxCommandProcessor  {
 public:
 	ofxCommandProcessor();
@@ -18,21 +14,10 @@ public:
 	void clear();
 	bool isReady();
 	void update();
-/*
-	void operator()() {
-		std::cout << "ofxCommanProcessor() operator: ()" << std::endl;
-		run();
-	}
-	*/
-	//void start();
-	//void run();
+	void shutdown();
 protected:
-	//boost::shared_ptr<boost::thread> thread_;
-	//boost::thread thread_;
 	std::deque<ofxCommand*>  queue;
-	//virtual void threadedFunction();
 	ofxCommand* take();
-	//boost::mutex mutex;
-	//bool is_running;
 };
+
 #endif
